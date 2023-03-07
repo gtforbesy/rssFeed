8cfd0ad74f64e530bbade0d068ef0c8c1a1d08f0
Tyler Forbes <tyler.forbes@cognizant.com>
2023-03-07 10:09:53 -0600

    initil commit

---
+++ src/main/java/com/nyt/rssFeed/RssFeedApplication.java
@@ -0,0 +1,13 @@
+package com.nyt.rssFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedApplication.class, args);
+	}
+
+}
